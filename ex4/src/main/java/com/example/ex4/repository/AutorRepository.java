8d8c9fcd7fbec729dbca2acccfaf3cd292a9de23
Danilo Mori <danilomori@Danilos-MacBook-Air.local>
2026-03-22 19:59:41 -0300

    commit2

---
+++ ex4/src/main/java/com/example/ex4/repository/AutorRepository.java
@@ -0,0 +1,10 @@
+package com.example.ex4.repository;
+
+import com.example.ex4.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+}
